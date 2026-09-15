95028b93e30de5e264763b34218a7021c4299b9a
Alicerumiho <a1078473622@gmail.com>
2026-09-16 00:10:41 +0800

    kernel: sing-box 1.14.1-neko-1 (KanadeAlice/sing-box = upstream v1.14.1 + neko patches); align libcore deps

--- buildScript/lib/core/get_source_env.sh
+++ buildScript/lib/core/get_source_env.sh
@@ -1,3 +1,3 @@
-# sing-box: MatsuriDayo fork 1.14.x tip = "1.14.0-neko-1" (commit 0bd0381c, 2026-09-03); was 1.12.9-neko-1 (aed32ee)
-export COMMIT_SING_BOX="0bd0381c146042237abacc831249a90ab753ef8e"
+# sing-box: KanadeAlice/sing-box branch 1.14.1-neko = upstream v1.14.1 + neko patches ("1.14.1-neko-1"); was MatsuriDayo 1.14.0-neko-1 (0bd0381c)
+export COMMIT_SING_BOX="8d88ff86437eedec58a745f6df689d5cbc1999f7"
 export COMMIT_LIBNEKO="1c47a3af71990a7b2192e03292b4d246c308ef0b"
